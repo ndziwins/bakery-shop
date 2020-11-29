000683292314bcb2f4516b7ac52865a4a1fdfd58
Norbert Dziwinski <norbert.dziwinski@mayeryn.com>
2020-11-29 15:56:39 +0100

    add fade to slider

--- index.php
+++ index.php
@@ -15,23 +15,40 @@ if ((isset($_SESSION['isLogged'])) && ($_SESSION['isLogged'] = true)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bakery Shop</title>
-
+    <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="timer.js"></script>
 
     <script type="text/javascript">
+        var number = Math.floor(Math.random() * 5) + 1;
+        var timer1 = 0;
+        var timer2 = 0;
 
-    var numer = Math.floor(Math.random()*5)+1;
+        function setSlide(slideNo) {
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+            number = slideNo - 1;
 
-    function changeSlide()
-    {
-        numer++; if (numer>5) numer=1;
-				
-        var file = "<img src=\"slides/slide" + numer + ".jpg\" />";
-        document.getElementById("slider").innerHTML = file;
+            fade();
+            setTimeout("changeSlide()", 500);
 
-        setTimeout("changeSlide()", 2000);
-    }
+        }
 
+        function fade() {
+            $("#slider").fadeOut(500);
+        }
+
+        function changeSlide() {
+            number++;
+            if (number > 5) number = 1;
+
+            var file = "<img src=\"slides/slide" + number + ".jpg\" />";
+            document.getElementById("slider").innerHTML = file;
+
+            $("#slider").fadeIn(500);
+
+            timer1 = setTimeout("changeSlide()", 4000);
+            timer2 = setTimeout("fade()", 3500);
+        }
     </script>
 
 </head>
@@ -42,6 +59,11 @@ if ((isset($_SESSION['isLogged'])) && ($_SESSION['isLogged'] = true)) {
     <br><br>
 
     <div id="slider"></div>
+    <span onclick="setSlide(1)" style="cursor:pointer;">[ 1 ]</span>
+    <span onclick="setSlide(2)" style="cursor:pointer;">[ 2 ]</span>
+    <span onclick="setSlide(3)" style="cursor:pointer;">[ 3 ]</span>
+    <span onclick="setSlide(4)" style="cursor:pointer;">[ 4 ]</span>
+    <span onclick="setSlide(5)" style="cursor:pointer;">[ 5 ]</span>
 
     <br><br>
 
